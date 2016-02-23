74e00eb29edbf6a83dbdb6891e524a8becffb7e5
RothGo <roth1002@gmail.com>
2016-02-23 17:09:31 +0800

    rackt -> reactjs

--- src/components/Provider.js
+++ src/components/Provider.js
@@ -14,7 +14,7 @@ function warnAboutReceivingStore() {
       '<Provider> does not support changing `store` on the fly. ' +
       'It is most likely that you see this error because you updated to ' +
       'Redux 2.x and React Redux 2.x which no longer hot reload reducers ' +
-      'automatically. See https://github.com/rackt/react-redux/releases/' +
+      'automatically. See https://github.com/reactjs/react-redux/releases/' +
       'tag/v2.0.0 for the migration instructions.'
     )
   }
--- test/components/Provider.spec.js
+++ test/components/Provider.spec.js
@@ -96,7 +96,7 @@ describe('React', () => {
         '<Provider> does not support changing `store` on the fly. ' +
         'It is most likely that you see this error because you updated to ' +
         'Redux 2.x and React Redux 2.x which no longer hot reload reducers ' +
-        'automatically. See https://github.com/rackt/react-redux/releases/' +
+        'automatically. See https://github.com/reactjs/react-redux/releases/' +
         'tag/v2.0.0 for the migration instructions.'
       )
 
