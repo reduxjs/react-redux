32d6e0601ac16a6b66277908623c1c78b6ff75ef
Ratson <github@ratson.name>
2016-04-14 00:21:42 +0800

    Remove duplicated console check

--- src/components/Provider.js
+++ src/components/Provider.js
@@ -9,17 +9,13 @@ function warnAboutReceivingStore() {
   }
   didWarnAboutReceivingStore = true
 
-  /* eslint-disable no-console */
-  if (typeof console !== 'undefined' && typeof console.error === 'function') {
-    warning(
-      '<Provider> does not support changing `store` on the fly. ' +
-      'It is most likely that you see this error because you updated to ' +
-      'Redux 2.x and React Redux 2.x which no longer hot reload reducers ' +
-      'automatically. See https://github.com/reactjs/react-redux/releases/' +
-      'tag/v2.0.0 for the migration instructions.'
-    )
-  }
-  /* eslint-disable no-console */
+  warning(
+    '<Provider> does not support changing `store` on the fly. ' +
+    'It is most likely that you see this error because you updated to ' +
+    'Redux 2.x and React Redux 2.x which no longer hot reload reducers ' +
+    'automatically. See https://github.com/reactjs/react-redux/releases/' +
+    'tag/v2.0.0 for the migration instructions.'
+  )
 }
 
 export default class Provider extends Component {
