a8ee8396e513352d13726612396c644f6cebe8e7
Jim Bolla <github@jimbolla.com>
2016-06-16 19:33:58 -0400

    remove dispatchable()... this probably doesn't need to be a part of react-redux.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import Provider from './components/Provider'
 import connect from './components/connect'
 import connectAdvanced from './components/connectAdvanced'
 import createShallowEqualSelector from './utils/createShallowEqualSelector'
-import dispatchable from './utils/dispatchable'
 
 export {
   Provider,
   connect,
   connectAdvanced,
-  createShallowEqualSelector,
-  dispatchable 
+  createShallowEqualSelector
 }
