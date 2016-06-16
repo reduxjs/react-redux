e2ad6a4f5bd5c8d5a381279e021a61de303ed2ac
Jim Bolla <github@jimbolla.com>
2016-06-15 23:42:47 -0400

    add dispatchable to the exported functions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Provider from './components/Provider'
 import connect from './components/connect'
-import connectToStore from './components/connectToStore'
+import connectToStore, { dispatchable } from './components/connectToStore'
 
-export { Provider, connect, connectToStore }
+export { Provider, connect, connectToStore, dispatchable }
