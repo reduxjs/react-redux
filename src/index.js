c4f68ab44067c7c380749b9247ab24aa6ceea197
Jim Bolla <github@jimbolla.com>
2016-06-17 16:14:38 -0400

    Make buildSelector injectable into connectAdvanced as another option

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import Provider from './components/Provider'
 import connect from './components/connect'
 import connectAdvanced from './components/connectAdvanced'
+
+import buildSelector from './utils/buildSelector'
 import shallowEqual from './utils/shallowEqual'
 
 export {
   Provider,
   connect,
   connectAdvanced,
+
+  buildSelector,
   shallowEqual
 }
--- src/utils/buildSelector.js
+++ src/utils/buildSelector.js
@@ -3,45 +3,49 @@ import { createSelectorCreator, defaultMemoize } from 'reselect'
 import shallowEqual from '../utils/shallowEqual'
 
 export default function buildSelector({
-    displayName,
-    ref,
     selectorFactory,
+    dispatch,
+    getState,
+    ref,
     recomputationsProp,
-    shouldUseState,
-    store
+    ...options
   }) {
-  // wrap the source selector in a shallow equals because props objects with same properties are
-  // symantically equal to React... no need to re-render.
-  const selector = createSelectorCreator(defaultMemoize, shallowEqual)(
-
-    // get the source selector from the factory
-    selectorFactory({
-      // useful for selector factories to show in error messages
-      displayName,
-      // useful for selectors that want to bind action creators before returning their selector
-      dispatch: store.dispatch
-    }),
-    
-    // make a shallow copy so that mutations don't leak to the original selector. any additional
-    // mutations added to the mutateProps func below should be checked for here
-    ref || recomputationsProp
-      ? (result => ({ ...result }))
-      : (result => result)
-  )
-
+  // the final props obect is mutated directly instead of projecting into a new object to avoid
+  // some extra object creation and tracking.
+  const mightMutateProps = ref || recomputationsProp
   function mutateProps(props, before, recomputations) {
     if (before === recomputations) return
     if (ref) props.ref = ref
     if (recomputationsProp) props[recomputationsProp] = recomputations
   }
 
-  return function runSelector(ownProps) {
-    const before = selector.recomputations()
-    const state = shouldUseState ? store.getState() : null
-    const props = selector(state, ownProps, store.dispatch)
-    const recomputations = selector.recomputations()
+  // wrap the source selector in a shallow equals because props objects with same properties are
+  // semantically equal to React... no need to re-render.
+  const masterSelector = createSelectorCreator(defaultMemoize, shallowEqual)(
+    selectorFactory({
+      // useful for selecto factories that want to bind action creators before returning
+      // their selector
+      dispatch,
+      // additional options passed to buildSelector are passed along to the selectorFactory
+      ...options
+    }),
 
-    mutateProps(props, before, recomputations)
+    // make a shallow copy so that fields added by mutateProps don't leak to the original selector.
+    // this is especially important for 'ref' since that's a reference back to the component
+    // instance. a singleton memoized selector would then be holding a reference to the instance,
+    // preventing the instance from being garbage collected
+    mightMutateProps
+      ? (result => ({ ...result }))
+      : (result => result)
+  )
+
+  return function runSelector(ownProps) {
+    const before = masterSelector.recomputations()
+    const state = getState()
+    const props = masterSelector(state, ownProps, dispatch)
+    const recomputations = masterSelector.recomputations()
+
+    if (mightMutateProps) mutateProps(props, before, recomputations)
     return { props, recomputations }
   }
 }
