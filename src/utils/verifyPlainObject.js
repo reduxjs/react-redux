a6f5d28f85d52eadafef9871f35b87db69711147
Jim Bolla <github@jimbolla.com>
2016-06-17 20:16:04 -0400

    refactor+comment connect.js

---
+++ src/utils/verifyPlainObject.js
@@ -0,0 +1,20 @@
+import isPlainObject from 'lodash/isPlainObject'
+import warning from './warning'
+
+// verifies that the first execution of func returns a plain object
+export default function verifyPlainObject(displayName, methodName, func) {
+  if (process.env.NODE_ENV === 'production') return func
+  let hasVerified = false
+  return (...args) => {
+    const result = func(...args)
+    if (hasVerified) return result
+    hasVerified = true
+    if (!isPlainObject(result)) {
+      warning(
+        `${methodName}() in ${displayName} must return a plain object. ` +
+        `Instead received ${result}.`
+      )
+    }
+    return result
+  }
+}
