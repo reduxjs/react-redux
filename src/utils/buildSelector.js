03c62ef1c187a9a8993679a61a24dbaf17875997
Jim Bolla <github@jimbolla.com>
2016-06-19 11:43:57 -0400

    refactor buildSelector

--- src/utils/buildSelector.js
+++ src/utils/buildSelector.js
@@ -8,24 +8,8 @@ export default function buildSelector({
     recomputationsProp,
     ...options
   }) {
-  // the final props obect is mutated directly instead of projecting into a new object to avoid
-  // some extra object creation and tracking.
-  const mightMutateProps = ref || recomputationsProp
-  function mutateProps(newProps, recomputations) {
-    if (!mightMutateProps) return newProps
-
-    // make a shallow copy so that fields added by mutateProps don't leak to the original selector.
-    // this is especially important for 'ref' since that's a reference back to the component
-    // instance. a singleton memoized selector would then be holding a reference to the instance,
-    // preventing the instance from being garbage collected
-    const props = { ...newProps }
-    if (ref) props.ref = ref
-    if (recomputationsProp) props[recomputationsProp] = recomputations
-    return props
-  }
-
   const selector = selectorFactory({
-    // useful for selecto factories that want to bind action creators before returning
+    // useful for selector factories that want to bind action creators before returning
     // their selector
     dispatch,
     // additional options passed to buildSelector are passed along to the selectorFactory
@@ -33,18 +17,33 @@ export default function buildSelector({
   })
 
   let recomputations = 0
-  let selectedProps = undefined
+  let selectorProps = undefined
   let finalProps = undefined
+
+  const mightAddProps = ref || recomputationsProp
+  function getFinalProps() {
+    if (!mightAddProps) return selectorProps
+
+    // make a shallow copy so that fields added don't leak to the original selector.
+    // this is especially important for 'ref' since that's a reference back to the component
+    // instance. a singleton memoized selector would then be holding a reference to the instance,
+    // preventing the instance from being garbage collected, and that would be bad
+    const props = { ...selectorProps }
+    if (ref) props.ref = ref
+    if (recomputationsProp) props[recomputationsProp] = recomputations
+    return props
+  }
+
   return function runSelector(ownProps) {
     const state = getState()
     const newProps = selector(state, ownProps, dispatch)
 
     // wrap the source selector in a shallow equals because props objects with same properties are
-    // semantically equal to React... no need to re-render.
-    if (!selectedProps || !shallowEqual(selectedProps, newProps)) {
+    // semantically equal to React... no need to return a new object.
+    if (!selectorProps || !shallowEqual(selectorProps, newProps)) {
       recomputations++
-      selectedProps = newProps
-      finalProps = mutateProps(newProps, recomputations)
+      selectorProps = newProps
+      finalProps = getFinalProps()
     }
 
     return finalProps
