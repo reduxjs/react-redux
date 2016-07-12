9a819038c0358b297737c24ff2ca2474673c396c
Jim Bolla <github@jimbolla.com>
2016-07-11 21:25:51 -0400

    refactoring/optimizing connect+selectors

--- src/components/connect.js
+++ src/components/connect.js
@@ -1,8 +1,6 @@
-import flow from 'lodash/flow'
-
 import connectAdvanced from './connectAdvanced'
-import verifyPlainObject from '../utils/verifyPlainObject'
-import { createFinalPropsSelector } from '../selectors/getFinalProps'
+import makeImpurePropsSelector from '../selectors/makeImpurePropsSelector'
+import makePurePropsSelector from '../selectors/makePurePropsSelector'
 import defaultMapDispatchFactories from '../selectors/mapDispatchToProps'
 import defaultMapStateFactories from '../selectors/mapStateToProps'
 import { defaultMergeProps } from '../selectors/mergeProps'
@@ -15,34 +13,27 @@ import { defaultMergeProps } from '../selectors/mergeProps'
   First, buildOptions combines its args with some meta into an options object that's passed to
   connectAdvanced, which will pass a modified* version of that options object to selectorFactory.
 
-    *values added to options: dispatch, displayName, WrappedComponent
+    *values added to options: displayName, WrappedComponent
 
   Each time selectorFactory is called (whenever an instance of the component in connectAdvanced is
   constructed or hot reloaded), it uses the modified options object to build a selector function:
 
-    1. Create the getState selector by matching mapStateToProps to the mapStateFactories array
+    1. Convert mapStateToProps into a selector by matching it to the mapStateFactories array
        passed in from buildOptions.
 
        The default behaviors (from mapStateToProps.js) check mapStateToProps for a function
        or missing value. This could be overridden by supplying a custom value to the
        mapStateFactories property of connect's options argument
     
-    2. Create the getDispatch selector by matching mapDispatchToProps to the mapDispatchFactories
+    2. Convert mapDispatchToProps into a selector by matching it to the mapDispatchFactories
        array passed in from buildOptions.
 
        The default behaviors (from mapDispatchToProps.js) check mapDispatchToProps for a
-       function, object, or missing value. The could be overridden by supplying a custom
+       function, object, or missing value. This could be overridden by supplying a custom
        value to the mapDispatchFactories property of connect's options argument.
     
-    3. Wrap the getState, getDispatch, and mergeProps selectors in functions that check that their
-       return values are plain objects (on their first invocation.)
-
-       This is to inform the developer that they made a mistake coding the function the supplied
-       for any of mapStateToProps, mapDispatchToProps, or mergeProps.
-
-    4. Combine getState, getDispatch, and mergeProps selectors into the final props selector, by
-       passing the getState's results, getDispatch's results, and original props to mergeProps.
-       (See getFinalProps.js)
+    3. Combine mapStateToProps, mapDispatchToProps, and mergeProps selectors into either a pure
+       (makePurePropsSelector.js) or impure (makeImpurePropsSelector.js) final props selector.
 
   The resulting final props selector is called by the component instance whenever it receives new
   props or is notified by the store subscription.
@@ -60,14 +51,14 @@ export function buildOptions(
     // used to compute the Connect component's displayName from the wrapped component's displayName.
     getDisplayName: name => `Connect(${name})`,
 
-    // passed through to selectorFactory. defaults to the array of funcs returned in mapDispatch.js
-    // that determine the appropriate sub-selector to use for mapDispatchToProps, depending on
-    // whether it's a function, object, or missing.
+    // passed through to selectorFactory. defaults to the array of funcs returned in
+    // mapDispatchToProps.js that determine the appropriate sub-selector to use for
+    // mapDispatchToProps, depending on whether it's a function, object, or missing.
     mapDispatchFactories: defaultMapDispatchFactories,
 
-    // passed through to selectorFactory. defaults to the array of funcs returned in mapState.js
-    // that determine the appropriate sub-selector to use for mapStateToProps, depending on
-    // whether it's a function or missing.
+    // passed through to selectorFactory. defaults to the array of funcs returned in
+    // mapStateToProps.js that determine the appropriate sub-selector to use for
+    // mapStateToProps, depending on whether it's a function or missing.
     mapStateFactories: defaultMapStateFactories,
 
     // if true, the selector returned by selectorFactory will memoize its results, allowing
@@ -98,42 +89,20 @@ export function buildOptions(
   }
 }
 
-export function addStateAndDispatchSelectors(options) {
-  function match(factories) {
+export function selectorFactory(dispatch, options) {
+  function match(mapToProps, factories) {
     for (let i = factories.length - 1; i >= 0; i--) {
-      const selector = factories[i](options)
+      const selector = factories[i](mapToProps, options)
       if (selector) return selector
     }
     return undefined
   }
 
-  return {
-    ...options,
-    getState: match(options.mapStateFactories),
-    getDispatch: match(options.mapDispatchFactories)
-  }
-}
+  const mapStateToProps = match(options.mapStateToProps, options.mapStateFactories)
+  const mapDispatchToProps = match(options.mapDispatchToProps, options.mapDispatchFactories)
+  const factory = options.pure ? makePurePropsSelector : makeImpurePropsSelector
 
-export function wrapWithVerify({ getState, getDispatch, mergeProps, ...options }) {
-  const verify = (methodName, func) => {
-    const withVerify = verifyPlainObject(options.displayName, methodName, func)
-    withVerify.meta = func.meta
-    return withVerify
-  }
-  return {
-    ...options,
-    getState: verify('mapStateToProps', getState),
-    getDispatch: verify('mapDispatchToProps', getDispatch),
-    mergeProps: verify('mergeProps', mergeProps)
-  }
-}
-
-export function selectorFactory(dispatch, options) {
-  return flow(
-    addStateAndDispatchSelectors,
-    wrapWithVerify,
-    createFinalPropsSelector
-  )({ ...options, dispatch })
+  return factory(dispatch, { ...options, mapStateToProps, mapDispatchToProps })
 }
 
 export default function connect(...args) {
