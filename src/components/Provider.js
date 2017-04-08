40b589fa63475a07c532b5e7373ebb0f53797645
Charley DAVID <hello@calyh.re>
2017-04-08 11:20:20 -0300

    Fix deprecation warnings for react >= 15.5.0

--- src/components/Provider.js
+++ src/components/Provider.js
@@ -1,4 +1,5 @@
-import { Component, PropTypes, Children } from 'react'
+import { Component, Children } from 'react'
+import PropTypes from 'prop-types'
 import { storeShape, subscriptionShape } from '../utils/PropTypes'
 import warning from '../utils/warning'
 
--- src/utils/PropTypes.js
+++ src/utils/PropTypes.js
@@ -1,4 +1,4 @@
-import { PropTypes } from 'react'
+import PropTypes from 'prop-types'
 
 export const subscriptionShape = PropTypes.shape({
   trySubscribe: PropTypes.func.isRequired,
