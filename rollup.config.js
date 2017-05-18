a7a11fccf2b21bc7569bbea801bea07ab4460b96
Bogdan Chadkin <trysound@yandex.ru>
2017-05-18 07:25:13 +0300

    Bundle umd with rollup (#681)

---
+++ rollup.config.js
@@ -0,0 +1,47 @@
+import nodeResolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+import replace from 'rollup-plugin-replace'
+import commonjs from 'rollup-plugin-commonjs'
+import uglify from 'rollup-plugin-uglify'
+
+const env = process.env.NODE_ENV
+
+const config = {
+  entry: 'src/index.js',
+  external: [
+    'react',
+    'redux'
+  ],
+  globals: {
+    'react': 'React',
+    'redux': 'Redux'
+  },
+  format: 'umd',
+  moduleName: 'ReactRedux',
+  plugins: [
+    nodeResolve(),
+    babel({
+      exclude: '**/node_modules/**'
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(env)
+    }),
+    commonjs()
+  ]
+}
+
+if (env === 'production') {
+  config.plugins.push(
+    uglify({
+      compress: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        screw_ie8: true,
+        warnings: false
+      }
+    })
+  )
+}
+
+export default config
