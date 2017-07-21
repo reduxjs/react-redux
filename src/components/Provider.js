bd549fdeac9f7a4f8d5499fc7d057ce02b1f0eb8
15271936620 <2389651246@qq.com>
2017-07-21 13:16:45 +0800

    add  a router  import

--- src/components/Provider.js
+++ src/components/Provider.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types'
 import { storeShape, subscriptionShape } from '../utils/PropTypes'
 import warning from '../utils/warning'
 
+import {Browser} from 'react-dom-router'
+
 let didWarnAboutReceivingStore = false
 function warnAboutReceivingStore() {
   if (didWarnAboutReceivingStore) {
