be6daceda50cbfbef6e10887b0b0f99dc2aa83f2
Tom Crockett <pelotom@gmail.com>
2017-10-24 13:10:49 -0700

    Increase timeout for typescript definition test

--- test/typescript.spec.js
+++ test/typescript.spec.js
@@ -1,6 +1,6 @@
 import * as tt from 'typescript-definition-tester'
 
-describe('TypeScript definitions', function () {
+describe('TypeScript definitions', () => {
   it('should compile against index.d.ts', (done) => {
     tt.compileDirectory(
       __dirname + '/typescript',
@@ -8,5 +8,5 @@ describe('TypeScript definitions', function () {
       { strict: true, jsx: true },
       () => done()
     )
-  })
+  }).timeout(5000)
 })
