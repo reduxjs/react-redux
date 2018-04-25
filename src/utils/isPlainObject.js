be777614deff348c2c7b6f138c6d5366dbc7cea6
Tim Dorr <timdorr@users.noreply.github.com>
2018-04-24 22:41:35 -0400

    Replace isPlainObject with unmodified version from Redux

--- src/utils/isPlainObject.js
+++ src/utils/isPlainObject.js
@@ -3,14 +3,12 @@
  * @returns {boolean} True if the argument appears to be a plain object.
  */
 export default function isPlainObject(obj) {
-    if (typeof obj !== 'object' || obj === null) return false
-  
-    let prototypeOfObj = Object.getPrototypeOf(obj)
-    let proto = obj
-    while (Object.getPrototypeOf(proto) !== null) {
-      proto = Object.getPrototypeOf(proto)
-  
-    }
-  
-    return prototypeOfObj === proto
+  if (typeof obj !== 'object' || obj === null) return false
+
+  let proto = obj
+  while (Object.getPrototypeOf(proto) !== null) {
+    proto = Object.getPrototypeOf(proto)
   }
+
+  return Object.getPrototypeOf(obj) === proto
+}
