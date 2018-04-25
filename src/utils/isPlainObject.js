38a231b98158c54406c413f13b47ecb1ada98470
Matt Hargett <plaztiksyke@gmail.com>
2018-04-24 19:10:17 -0700

    Add missing files.

---
+++ src/utils/isPlainObject.js
@@ -0,0 +1,16 @@
+/**
+ * @param {any} obj The object to inspect.
+ * @returns {boolean} True if the argument appears to be a plain object.
+ */
+export default function isPlainObject(obj) {
+    if (typeof obj !== 'object' || obj === null) return false
+  
+    let prototypeOfObj = Object.getPrototypeOf(obj)
+    let proto = obj
+    while (Object.getPrototypeOf(proto) !== null) {
+      proto = Object.getPrototypeOf(proto)
+  
+    }
+  
+    return prototypeOfObj === proto
+  }
