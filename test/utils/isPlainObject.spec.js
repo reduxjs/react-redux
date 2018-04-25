96f3cd566e3845093a9d30ded7c2740ef8e2582b
Tim Dorr <timdorr@users.noreply.github.com>
2018-04-24 22:42:59 -0400

    Also replace the spec with the unmodified version

--- test/utils/isPlainObject.spec.js
+++ test/utils/isPlainObject.spec.js
@@ -1,5 +1,6 @@
 import isPlainObject from '../../src/utils/isPlainObject'
 import vm from 'vm'
+
 describe('isPlainObject', () => {
   it('returns true only if plain object', () => {
     function Test() {
@@ -12,9 +13,9 @@ describe('isPlainObject', () => {
     expect(isPlainObject(sandbox.fromAnotherRealm)).toBe(true)
     expect(isPlainObject(new Test())).toBe(false)
     expect(isPlainObject(new Date())).toBe(false)
-    expect(isPlainObject([ 1, 2, 3 ])).toBe(false)
+    expect(isPlainObject([1, 2, 3])).toBe(false)
     expect(isPlainObject(null)).toBe(false)
     expect(isPlainObject()).toBe(false)
-    expect(isPlainObject({ 'x': 1, 'y': 2 })).toBe(true)
+    expect(isPlainObject({ x: 1, y: 2 })).toBe(true)
   })
 })
