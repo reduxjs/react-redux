eee25a4b565748ea91aeb284ef510a698f78944f
Greg Beaver <greg.beaver.cello@gmail.com>
2018-08-02 22:13:20 -0400

    Implement testing in React 0.14, 15, 16, add wallaby.js, use enzyme (#984)
    * begin work
    
    * enable testing on 4 react versions on travis
    * enable local testing on 4 versions
    * enable running wallaby if installed
    
    * Fix tests in 0.14, 15, and 16
    
    * remove 15.4, not really necessary and it requires a tweak to the install script
    
    * fix 2 major potential issues
    
    1) by putting all the installs in setupTestEnv into a single npm command, npm --no-save becomes possible. When they are on separate lines, the 2nd npm i removes the previous line's work
    2) after test runs, we restore the node_modules to what devDependencies says
    
    * update contributing docs and add missing test:watch
    
    * re-work to use subdirectories for testing specific react versions
    
    caveat: collecting coverage does not work yet, we need to combine the lcov reports
    
    * fully working! with merged coverage!
    
    * fix linting, remove unnecessary file
    
    * fix test:watch
    
    * fix travis tests to run in parallel for each version
    
    * oops, didn't make travis run the CI test
    
    * sigh... npm syntax error
    
    * speed up test suites by only installing the specific version needed
    
    * remove unused plugin
    
    * simplify test script options in package.json
    
    use the REACT env variable to run tests for a specific version, set it to "all" to run for all supported versions
    
    * simpler gitignore
    
    * remove unnecessary coverage merging, codecov does that automagically
    
    * simplify test running
    
    * new docs on testing specific React versions
    
    * move scripts to test/, remove unused dep
    
    * revert unintentional cosmetic changes
    
    * add default version for "npm test"
    
    * revert unintentional cosmetic changes to test import order
    
    * restore the correct test renderer version
    
    * fix travis, add a note about the matrix needing update on adding a React version
    
    * Add cross-spawn dependency
    
    * Use cross-spawn for consistent NPM installations cross-platform

---
+++ test/install-test-deps.js
@@ -0,0 +1,86 @@
+/* eslint no-console: 0 */
+'use strict';
+
+const { readdirSync, existsSync, copyFile, mkdirSync } = require('fs');
+const rimraf = require('rimraf');
+const { join } = require('path');
+const spawn = require("cross-spawn");
+const reactVersion = process.env.REACT || '16.4'
+
+readdirSync(join(__dirname, 'react')).forEach(version => {
+  if (reactVersion.toLowerCase() !== 'all' && version !== reactVersion) {
+    console.log(`skipping ${version}, ${reactVersion} was specified`)
+    return
+  }
+  const tests = [join(__dirname, 'components'), join(__dirname, 'utils')]
+  const srcs = [
+    join(__dirname, '..', 'src', 'components'),
+    join(__dirname, '..', 'src', 'connect'),
+    join(__dirname, '..', 'src', 'utils'),
+  ]
+  const dest = [
+    join(__dirname, 'react', version, 'test', 'components'),
+    join(__dirname, 'react', version, 'test', 'utils'),
+  ]
+  const srcDest = [
+    join(__dirname, 'react', version, 'src', 'components'),
+    join(__dirname, 'react', version, 'src', 'connect'),
+    join(__dirname, 'react', version, 'src', 'utils'),
+  ]
+  if (!existsSync(join(__dirname, 'react', version, 'src'))) {
+    mkdirSync(join(__dirname, 'react', version, 'src'))
+  }
+  if (!existsSync(join(__dirname, 'react', version))) {
+    throw new Error(`getTestDeps.js is missing from react version ${version}'s "test" directory, cannot run tests`)
+  }
+  console.log('Copying test files')
+  tests.forEach((dir, i) => {
+    if (existsSync(dest[i])) {
+      console.log('clearing old tests in ' + dest[i])
+      rimraf.sync(join(dest[i], '*'))
+    } else {
+      mkdirSync(dest[i])
+    }
+    readdirSync(dir).forEach(file => {
+      copyFile(join(tests[i], file), join(dest[i], file), e => {
+        if (e) console.log(e)
+      })
+    })
+  })
+  console.log('Copying source files')
+  srcs.forEach((dir, i) => {
+    if (existsSync(srcDest[i])) {
+      console.log('clearing old sources in ' + srcDest[i])
+      rimraf.sync(join(srcDest[i], '*'))
+    } else {
+      if (!existsSync(join(__dirname, 'react', version, 'src'))) {
+        mkdirSync(join(__dirname, 'react', version, 'src'))
+      }
+      mkdirSync(srcDest[i])
+    }
+    readdirSync(dir).forEach(file => {
+      copyFile(join(srcs[i], file), join(srcDest[i], file), e => {
+        if (e) console.log(e)
+      })
+    })
+    copyFile(join(__dirname, '..', 'src', 'index.js'), join(__dirname, 'react', version, 'src', 'index.js'), e => {
+      if (e) console.log(e)
+    })
+  })
+  const cwd = join(__dirname, 'react', version);
+  if (existsSync(join(__dirname, 'react', version, 'node_modules', 'react', 'package.json'))) {
+    console.log(`Skipping React version ${version} ... (already installed)`);
+    return
+  }
+
+  console.log(`Installing React version ${version}...`);
+
+  const installTask = spawn.sync('npm', ['install'], {
+    cwd,
+    stdio: 'inherit',
+  });
+
+  if (installTask.status > 0) {
+    process.exit(installTask.status);
+  }
+});
