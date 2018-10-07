7fba8b7839a3b1eaeaef0fb8d58f2aa9f42e420f
Mark Erikson <mark@isquaredsoftware.com>
2018-10-07 10:40:11 -0400

    Try switching theme to Monokai

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -61,7 +61,7 @@ const siteConfig = {
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: "atom-one-dark"
+    theme: "monokai"
   },
 
   // Add custom scripts here that would be placed in <script> tags.
