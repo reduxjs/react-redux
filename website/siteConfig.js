e649b0ec77dfd5735b331537beff5e2bfe0450ad
sveinpg <sveinpg.gmail.com>
2018-10-07 10:09:42 +0200

    Changed highlight theme and updated snippet language

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -61,7 +61,7 @@ const siteConfig = {
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: "default"
+    theme: "atom-one-dark"
   },
 
   // Add custom scripts here that would be placed in <script> tags.
