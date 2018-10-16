cdbc49e74569964ed07048794b925c1d9e03ff2b
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-10-16 16:38:09 +0200

    Simplify test script

--- test/run-tests.js
+++ test/run-tests.js
@@ -2,7 +2,7 @@ const npmRun = require('npm-run')
 const LATEST_VERSION = '16.5'
 const version = process.env.REACT || LATEST_VERSION
 
-const jestConfig = {
+let jestConfig = {
   testURL: 'http://localhost',
   collectCoverage: true,
   coverageDirectory: `${__dirname}/coverage`,
@@ -14,21 +14,17 @@ const jestConfig = {
 require('./install-test-deps.js')
 
 if (version.toLowerCase() === 'all') {
-  const allVersionsConfig = {
+  jestConfig = {
     ...jestConfig,
     rootDir: __dirname,
     // every directory has the same coverage, so we collect it only from one
     collectCoverageFrom: [`react/${LATEST_VERSION}/src/**.js`]
   }
-  npmRun.execSync(`node ./node_modules/.bin/jest -c '${JSON.stringify(allVersionsConfig)}'`, { stdio: 'inherit' })
 } else {
-  try {
-    const specificVersionConfig = {
-      ...jestConfig,
-      rootDir: `${__dirname}/react/${version}`,
-    }
-    npmRun.execSync(`cd test/react/${version} && npm test -- -c '${JSON.stringify(specificVersionConfig)}'`, { stdio: 'inherit' })
-  } finally {
-    npmRun.execSync('cd ../../..')
+  jestConfig = {
+    ...jestConfig,
+    rootDir: `${__dirname}/react/${version}`,
   }
 }
+
+npmRun.execSync(`jest -c '${JSON.stringify(jestConfig)}'`, { stdio: 'inherit' })
