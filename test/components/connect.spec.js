ec1cf26b65d67daac8a7b4f20850e92882dd2595
Tim Dorr <timdorr@users.noreply.github.com>
2018-10-25 10:33:55 -0400

    Wrap the test in a check for React.memo

--- test/components/connect.spec.js
+++ test/components/connect.spec.js
@@ -1,6 +1,6 @@
 /*eslint-disable react/prop-types*/
 
-import React, { Children, Component, memo } from 'react'
+import React, { Children, Component } from 'react'
 import createClass from 'create-react-class'
 import PropTypes from 'prop-types'
 import ReactDOM from 'react-dom'
@@ -108,18 +108,19 @@ describe('React', () => {
       expect(tester.getByTestId('hi')).toHaveTextContent('there')
     })
 
-    // TODO: test that fails in all but 16.6 because it's new and only exists there.
-    it.skip('Should work with a memo component', () => {
-      const store = createStore(() => ({ hi: 'there' }))
+    it('Should work with a memo component, if it exists', () => {
+      if (React.memo) {
+        const store = createStore(() => ({ hi: 'there' }))
 
-      const Container = memo((props) => <Passthrough {...props} />)
-      const WrappedContainer = connect(state => state)(Container);
+        const Container = React.memo((props) => <Passthrough {...props} />)
+        const WrappedContainer = connect(state => state)(Container);
 
-      const tester = rtl.render(<ProviderMock store={store}>
-          <WrappedContainer pass="through" />
-      </ProviderMock>)
+        const tester = rtl.render(<ProviderMock store={store}>
+            <WrappedContainer pass="through" />
+        </ProviderMock>)
 
-      expect(tester.getByTestId('hi')).toHaveTextContent('there')
+        expect(tester.getByTestId('hi')).toHaveTextContent('there')
+      }
     });
 
     it('should pass state and props to the given component', () => {
