8a56e418ecd76db6eaa524c137a7d85e3d16026e
Tim Dorr <git@timdorr.com>
2018-11-19 11:07:40 -0500

    Add a test for the new context API too.

--- test/components/LegacyProvider.spec.js
+++ test/components/LegacyProvider.spec.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { LegacyProvider } from '../../src/index.js'
-import { createStore } from 'redux'
 import * as rtl from 'react-testing-library'
 import 'jest-dom/extend-expect'
+
+import { createStore } from 'redux'
+import { LegacyProvider } from '../../src/index.js'
+import { ReactReduxContext } from '../../src/components/Context'
 
 const createExampleTextReducer = () => (state = 'example text') => state
 
 describe('LegacyProvider', () => {
+  afterEach(() => rtl.cleanup())
+
   it('provides a legacy context', () => {
     class LegacyChild extends Component {
       render() {
@@ -33,6 +37,33 @@ describe('LegacyProvider', () => {
     const tester = rtl.render(
       <LegacyProvider store={store}>
         <LegacyChild />
+      </LegacyProvider>
+    )
+    expect(spy).toHaveBeenCalledTimes(0)
+    spy.mockRestore()
+
+    expect(tester.getByTestId('store')).toHaveTextContent('example text')
+  })
+
+  it('passes through everything to the new Provider and provides the new Context as well', () => {
+    class Child extends Component {
+      render() {
+        return (
+          <ReactReduxContext.Consumer>
+            {({ storeState }) => {
+              return <div data-testid="store">{storeState}</div>
+            }}
+          </ReactReduxContext.Consumer>
+        )
+      }
+    }
+
+    const store = createStore(createExampleTextReducer())
+
+    const spy = jest.spyOn(console, 'error').mockImplementation(() => {})
+    const tester = rtl.render(
+      <LegacyProvider store={store}>
+        <Child />
       </LegacyProvider>
     )
     expect(spy).toHaveBeenCalledTimes(0)
