ad20466b1c02088327a4e61eae2beb0495821b15
Wei Gao <wgao19@gmail.com>
2018-12-20 22:34:35 +0800

    Fix `siteConfig.js`

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -34,7 +34,7 @@ const siteConfig = {
       doc: "using-react-redux/connect-mapstate",
       label: "Using React-Redux"
     },
-    { doc: "api", label: "API" }
+    { doc: "api/connect", label: "API" },
     { href : "https://www.github.com/reduxjs/react-redux", label : "Github"},
     { href: "/introduction/quick-start#help-and-discussion", label: "Need help?" },
   ],
