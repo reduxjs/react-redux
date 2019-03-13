593592e6264da112944ee7c4f437e33686010033
Mark Erikson <mark@isquaredsoftware.com>
2019-03-12 22:19:57 -0400

    Inject unstable_batchedUpdates in default entry point

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ import connectAdvanced from './components/connectAdvanced'
 import { ReactReduxContext } from './components/Context'
 import connect from './connect/connect'
 
-export { Provider, connectAdvanced, ReactReduxContext, connect }
+import { setBatch } from './utils/batch'
+import { unstable_batchedUpdates as batch } from './utils/reactBatchedUpdates'
+
+setBatch(batch)
+
+export { Provider, connectAdvanced, ReactReduxContext, connect, batch }
