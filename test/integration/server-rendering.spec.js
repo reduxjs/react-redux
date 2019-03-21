b578e963e5a334019acef2ba87b0d3af93c32d85
Mark Erikson <mark@isquaredsoftware.com>
2019-03-21 19:53:54 -0400

    Force SSR tests to mimic a Node environment

--- test/integration/server-rendering.spec.js
+++ test/integration/server-rendering.spec.js
@@ -1,3 +1,10 @@
+/**
+ * @jest-environment node
+ * Set this so that `window` is undefined to correctly mimic a Node SSR scenario.
+ * That allows connectAdvanced to fall back to `useEffect` instead of `useLayoutEffect`
+ * to avoid ugly console warnings when used with SSR.
+ */
+
 /*eslint-disable react/prop-types*/
 
 import React from 'react'
