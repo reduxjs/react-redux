b51d7364c15931719bcc8ebea87333d8e1b72a7c
Jason Galea <jason@Jasons-MBP.fritz.box>
2019-03-22 23:19:59 +1000

    Add a test to verify batched dispatches result in a single render

---
+++ test/utils/batch.spec.js
@@ -0,0 +1,94 @@
+/*eslint-disable react/prop-types*/
+
+import React from 'react'
+import { createStore } from 'redux'
+import { Provider as ProviderMock, batch, connect } from '../../src/index.js'
+import * as rtl from 'react-testing-library'
+import 'jest-dom/extend-expect'
+
+describe('React', () => {
+  describe('batch', () => {
+    afterEach(() => rtl.cleanup())
+
+    it('should render only once for batched dispatches', () => {
+      const store = createStore((state, action) => {
+        let newState =
+          state !== undefined
+            ? state
+            : {
+                foo: '',
+                bar: ''
+              }
+        switch (action.type) {
+          case 'FOO':
+            newState = {
+              ...newState,
+              foo: action.payload
+            }
+            break
+          case 'BAR':
+            newState = {
+              ...newState,
+              bar: action.payload
+            }
+            break
+        }
+        return newState
+      })
+
+      const mapStateSpy = jest.fn()
+      const renderSpy = jest.fn()
+
+      const componentDecorator = connect(state => {
+        mapStateSpy()
+
+        return {
+          foo: state.foo,
+          bar: state.bar,
+          baz: state.baz
+        }
+      })
+
+      const component = () => {
+        renderSpy()
+        return <div>Hello</div>
+      }
+
+      const Component = componentDecorator(component)
+
+      rtl.render(
+        <ProviderMock store={store}>
+          <Component />
+        </ProviderMock>
+      )
+
+      // 1. Initial render
+      expect(mapStateSpy).toHaveBeenCalledTimes(1)
+      expect(renderSpy).toHaveBeenCalledTimes(1)
+
+      rtl.act(() => {
+        store.dispatch({ type: 'FOO', payload: 1 })
+      })
+      rtl.act(() => {
+        store.dispatch({ type: 'BAR', payload: 1 })
+      })
+
+      // 2. Separate store dispatches (sanity check)
+      expect(mapStateSpy).toHaveBeenCalledTimes(3)
+      expect(renderSpy).toHaveBeenCalledTimes(3)
+
+      batch(() => {
+        rtl.act(() => {
+          store.dispatch({ type: 'FOO', payload: 2 })
+        })
+        rtl.act(() => {
+          store.dispatch({ type: 'BAR', payload: 2 })
+        })
+      })
+
+      // 2. Batched store dispatches
+      expect(mapStateSpy).toHaveBeenCalledTimes(5)
+      expect(renderSpy).toHaveBeenCalledTimes(4)
+    })
+  })
+})
