cfcd2b37f91b487d49198256a5737d08ddb92a75
Jason Galea <jason@Jasons-MBP.fritz.box>
2019-03-23 12:19:33 +1000

    Add test to verify defaultNoopBatch has no effect on renders (failing)

--- test/utils/batch.spec.js
+++ test/utils/batch.spec.js
@@ -3,6 +3,8 @@
 import React from 'react'
 import { createStore } from 'redux'
 import { Provider as ProviderMock, batch, connect } from '../../src/index.js'
+import { batch as defaultNoopBatch } from '../../src/alternate-renderers.js'
+
 import * as rtl from 'react-testing-library'
 import 'jest-dom/extend-expect'
 
@@ -78,6 +80,80 @@ describe('React', () => {
       // 2. Batched store dispatches
       expect(mapStateSpy).toHaveBeenCalledTimes(5)
       expect(renderSpy).toHaveBeenCalledTimes(4)
+    })
+  })
+
+  describe('defaultNoopBatch', () => {
+    afterEach(() => rtl.cleanup())
+
+    it('should call the dispatch callback', () => {
+      const initialState = { foo: '', bar: '' }
+      const reducer = (state = initialState, action) => {
+        switch (action.type) {
+          case 'FOO':
+            state = { ...state, foo: action.payload }
+            break
+          case 'BAR':
+            state = { ...state, bar: action.payload }
+            break
+        }
+        return state
+      }
+      const store = createStore(reducer)
+
+      const mapStateSpy = jest.fn()
+      const renderSpy = jest.fn()
+
+      const componentDecorator = connect(state => {
+        mapStateSpy()
+
+        return {
+          foo: state.foo,
+          bar: state.bar,
+          baz: state.baz
+        }
+      })
+
+      const component = () => {
+        renderSpy()
+        return <div>Hello</div>
+      }
+
+      const Component = componentDecorator(component)
+
+      rtl.render(
+        <ProviderMock store={store}>
+          <Component />
+        </ProviderMock>
+      )
+
+      // 1. Initial render
+      expect(mapStateSpy).toHaveBeenCalledTimes(1)
+      expect(renderSpy).toHaveBeenCalledTimes(1)
+
+      rtl.act(() => {
+        store.dispatch({ type: 'FOO', payload: 1 })
+      })
+      rtl.act(() => {
+        store.dispatch({ type: 'BAR', payload: 1 })
+      })
+
+      // 2. Separate store dispatches (sanity check)
+      expect(mapStateSpy).toHaveBeenCalledTimes(3)
+      expect(renderSpy).toHaveBeenCalledTimes(3)
+
+      defaultNoopBatch(() => {
+        rtl.act(() => {
+          store.dispatch({ type: 'FOO', payload: 2 })
+        })
+        rtl.act(() => {
+          store.dispatch({ type: 'BAR', payload: 2 })
+        })
+      })
+
+      // 2. Batched store dispatches
+      expect(mapStateSpy).toHaveBeenCalledTimes(5)
+      expect(renderSpy).toHaveBeenCalledTimes(5)
     })
   })
 })
