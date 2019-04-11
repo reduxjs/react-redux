c718c5b94016de41be9420ec82aa0b7817760153
Kamil Badyla <kamilbadyla@gmail.com>
2019-04-11 13:07:03 +0200

    Export subscription

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import connectAdvanced from './components/connectAdvanced'
 import { ReactReduxContext } from './components/Context'
 import connect from './connect/connect'
 
+import Subscription from './utils/Subscription'
 import { setBatch } from './utils/batch'
 import { unstable_batchedUpdates as batch } from './utils/reactBatchedUpdates'
 
 setBatch(batch)
 
-export { Provider, connectAdvanced, ReactReduxContext, connect, batch }
+export {
+  Provider,
+  connectAdvanced,
+  ReactReduxContext,
+  connect,
+  batch,
+  Subscription
+}
