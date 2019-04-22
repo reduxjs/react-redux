15ef9b99a3f7ee4206c6fd2714db1e9916b91058
Jonathan Ziller <jonathan.ziller@gmail.com>
2019-04-22 05:15:08 +0200

    add react hooks for accessing redux store state and dispatching redux actions (#1248)
    * add react hooks for accessing redux store state and dispatching redux actions
    
    * remove `useReduxContext` from public API
    
    * add `useRedux` hook
    
    * Preserve stack trace of errors inside store subscription callback
    
    Ported changes from react-redux-hooks-poc
    
    Note: the "transient errors" test seems flawed atm.
    
    * Alter test descriptions to use string names
    
    WebStorm won't recognize tests as runnable if `someFunc.name` is
    used as the `describe()` argument.

---
+++ src/hooks/useDispatch.js
@@ -0,0 +1,31 @@
+import { useStore } from './useStore'
+
+/**
+ * A hook to access the redux `dispatch` function. Note that in most cases where you
+ * might want to use this hook it is recommended to use `useActions` instead to bind
+ * action creators to the `dispatch` function.
+ * 
+ * @returns {any} redux store's `dispatch` function
+ *
+ * Usage:
+ *
+```jsx
+import React, { useCallback } from 'react'
+import { useReduxDispatch } from 'react-redux'
+
+export const CounterComponent = ({ value }) => {
+  const dispatch = useDispatch()
+  const increaseCounter = useCallback(() => dispatch({ type: 'increase-counter' }), [])
+  return (
+    <div>
+      <span>{value}</span>
+      <button onClick={increaseCounter}>Increase counter</button>
+    </div>
+  )
+}
+```
+ */
+export function useDispatch() {
+  const store = useStore()
+  return store.dispatch
+}
