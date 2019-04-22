9b6629d5b22d151ddadbc9dd554f4b782dc5706f
Jonathan Ziller <jonathan.ziller@gmail.com>
2019-04-22 05:15:08 +0200

    add react hooks for accessing redux store state and dispatching redux actions (#1248)
    * add react hooks for accessing redux store state and dispatching redux actions
    
    * remove `useReduxContext` from public API
    
    * add `useRedux` hook
    
    * Preserve stack trace of errors inside store subscription callback
    
    Ported changes from react-redux-hooks-poc
    
    Note: the "transient errors" test seems flawed atm.
    
    * Alter test descriptions to use string names
    
    WebStorm won't recognize tests as runnable if `someFunc.name` is
    used as the `describe()` argument.

--- src/alternate-renderers.js
+++ src/alternate-renderers.js
@@ -3,9 +3,26 @@ import connectAdvanced from './components/connectAdvanced'
 import { ReactReduxContext } from './components/Context'
 import connect from './connect/connect'
 
+import { useActions } from './hooks/useActions'
+import { useDispatch } from './hooks/useDispatch'
+import { useRedux } from './hooks/useRedux'
+import { useSelector } from './hooks/useSelector'
+import { useStore } from './hooks/useStore'
+
 import { getBatch } from './utils/batch'
 
 // For other renderers besides ReactDOM and React Native, use the default noop batch function
 const batch = getBatch()
 
-export { Provider, connectAdvanced, ReactReduxContext, connect, batch }
+export {
+  Provider,
+  connectAdvanced,
+  ReactReduxContext,
+  connect,
+  batch,
+  useActions,
+  useDispatch,
+  useRedux,
+  useSelector,
+  useStore
+}
--- src/index.js
+++ src/index.js
@@ -3,9 +3,26 @@ import connectAdvanced from './components/connectAdvanced'
 import { ReactReduxContext } from './components/Context'
 import connect from './connect/connect'
 
+import { useActions } from './hooks/useActions'
+import { useDispatch } from './hooks/useDispatch'
+import { useRedux } from './hooks/useRedux'
+import { useSelector } from './hooks/useSelector'
+import { useStore } from './hooks/useStore'
+
 import { setBatch } from './utils/batch'
 import { unstable_batchedUpdates as batch } from './utils/reactBatchedUpdates'
 
 setBatch(batch)
 
-export { Provider, connectAdvanced, ReactReduxContext, connect, batch }
+export {
+  Provider,
+  connectAdvanced,
+  ReactReduxContext,
+  connect,
+  batch,
+  useActions,
+  useDispatch,
+  useRedux,
+  useSelector,
+  useStore
+}
