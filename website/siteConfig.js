9030f138a20ade49ab47835e01e70d6c4f0f292a
Mark Erikson <mark@isquaredsoftware.com>
2019-04-22 01:26:11 -0400

    Formatting

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -9,11 +9,11 @@
 // site configuration options.
 
 const siteConfig = {
-  title: "React Redux", // Title for your website.
-  tagline: "Official React bindings for Redux",
-  url: "https://react-redux.js.org", // Your website URL
-  baseUrl: "/",
-  docsUrl : "",
+  title: 'React Redux', // Title for your website.
+  tagline: 'Official React bindings for Redux',
+  url: 'https://react-redux.js.org', // Your website URL
+  baseUrl: '/',
+  docsUrl: '',
   algolia: {
     apiKey: '2d058d216b7fd5d68d481fd48ee72c06',
     indexName: 'react-redux',
@@ -21,36 +21,39 @@ const siteConfig = {
   },
 
   // Used for publishing and more
-  projectName: "react-redux",
-  organizationName: "reduxjs",
+  projectName: 'react-redux',
+  organizationName: 'reduxjs',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    { doc: "introduction/quick-start", label: "Quick Start" },
-    { doc: "using-react-redux/connect-mapstate", label: "Using React Redux"},
-    { doc: "api/connect", label: "API" },
-    { href : "https://www.github.com/reduxjs/react-redux", label : "Github"},
-    { href: "/introduction/quick-start#help-and-discussion", label: "Need help?" },
+    { doc: 'introduction/quick-start', label: 'Quick Start' },
+    { doc: 'using-react-redux/connect-mapstate', label: 'Using React Redux' },
+    { doc: 'api/connect', label: 'API' },
+    { href: 'https://www.github.com/reduxjs/react-redux', label: 'Github' },
+    {
+      href: '/introduction/quick-start#help-and-discussion',
+      label: 'Need help?'
+    }
   ],
 
   /* path to images for header/footer */
-  headerIcon: "img/redux_white.svg",
-  footerIcon: "img/redux_white.svg",
-  favicon: "img/favicon/favicon.ico",
+  headerIcon: 'img/redux_white.svg',
+  footerIcon: 'img/redux_white.svg',
+  favicon: 'img/favicon/favicon.ico',
 
   /* Colors for website */
   colors: {
-    primaryColor: "#764ABC",
-    secondaryColor: "#40216F",
-    accentColor1: "#717171",
-    accentColor2: "#F3EAFF",
-    accentColor3: "#D2B9F3",
-    accentColor4: "#ECF4F9",
-    accentColor5: "#CBDDEA",
-    accentColor6: "#2F5773"
+    primaryColor: '#764ABC',
+    secondaryColor: '#40216F',
+    accentColor1: '#717171',
+    accentColor2: '#F3EAFF',
+    accentColor3: '#D2B9F3',
+    accentColor4: '#ECF4F9',
+    accentColor5: '#CBDDEA',
+    accentColor6: '#2F5773'
   },
 
   /* Custom fonts for website */
@@ -68,36 +71,36 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: 'Copyright (c) 2015-present Dan Abramov and the Redux documentation authors.',
+  copyright:
+    'Copyright (c) 2015-present Dan Abramov and the Redux documentation authors.',
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: "monokai"
+    theme: 'monokai'
   },
 
   // Add custom scripts here that would be placed in <script> tags.
   scripts: [
-    "/scripts/sidebarScroll.js",
-    "/scripts/codeblock.js",
-    "https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js",
-    "https://buttons.github.io/buttons.js",
+    '/scripts/sidebarScroll.js',
+    '/scripts/codeblock.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js',
+    'https://buttons.github.io/buttons.js'
   ],
-
 
   enableUpdateTime: true,
   // On page navigation for the current documentation page.
-  onPageNav: "separate",
+  onPageNav: 'separate',
   // No .html extensions for paths.
   cleanUrl: true,
 
   // Open Graph and Twitter card images.
-  ogImage: "img/redux-logo-landscape.png",
-  twitterImage: "img/redux-logo-twitter.png",
+  ogImage: 'img/redux-logo-landscape.png',
+  twitterImage: 'img/redux-logo-twitter.png',
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
-  repoUrl: "https://github.com/reduxjs/react-redux",
-  
+  repoUrl: 'https://github.com/reduxjs/react-redux',
+
   /**
    * Note:
    * This will generate a link on the versioned docs page for "pre-release versions"
@@ -106,8 +109,8 @@ const siteConfig = {
    * More info: https://docusaurus.io/docs/en/versioning
    */
   // nextVersion: "7.x",
-  
-  gaTrackingId : "UA-130598673-2",
-};
 
-module.exports = siteConfig;
+  gaTrackingId: 'UA-130598673-2'
+}
+
+module.exports = siteConfig
