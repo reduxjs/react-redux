92baa0474914c52bdae5a146dad4deee3301ed8b
Jonathan Ziller <jonathan.ziller@gmail.com>
2019-04-23 20:17:19 +0200

    use proper example code tag in hooks JSDoc comments (#1257)
    * use proper example code tag in hooks JSDoc comments
    
    * fix mistake in `useActions` hook example code
    
    * remove TypeScript annotations from example code and adjust `useReduxContext` hook to also use @example JSDoc tag

--- src/hooks/useRedux.js
+++ src/hooks/useRedux.js
@@ -2,38 +2,41 @@ import { useSelector } from './useSelector'
 import { useActions } from './useActions'
 
 /**
- * A hook to access the redux store's state and to bind action creators to 
+ * A hook to access the redux store's state and to bind action creators to
  * the store's dispatch function. In essence, this hook is a combination of
  * `useSelector` and `useActions`.
- * 
+ *
+ * Note that this hook does currently not allow to pass a dependencies array,
+ * so the passed selector and any created callbacks are not memoized. If you
+ * require memoization, please use `useActions` and `useSelector`.
+ *
  * @param {Function} selector the selector function
  * @param {Function|Function[]|Object.<string, Function>} actions the action creators to bind
- * 
+ *
  * @returns {[any, any]} a tuple of the selected state and the bound action creators
  *
- * Usage:
+ * @example
  *
-```jsx
-import React from 'react'
-import { useRedux } from 'react-redux'
-
-export const CounterComponent = () => {
-  const [counter, { inc1, inc }] = useRedux(state => state.counter, {
-    inc1: () => ({ type: 'inc1' }),
-    inc: amount => ({ type: 'inc', amount }),
-  })
-
-  return (
-    <>
-      <div>
-        {counter}
-      </div>
-      <button onClick={inc1}>Increment by 1</button>
-      <button onClick={() => inc(5)}>Increment by 5</button>
-    </>
-  )
-}
-```
+ * import React from 'react'
+ * import { useRedux } from 'react-redux'
+ * import { RootState } from './store'
+ *
+ * export const CounterComponent = () => {
+ *   const [counter, { inc1, inc }] = useRedux(state => state.counter, {
+ *     inc1: () => ({ type: 'inc1' }),
+ *     inc: amount => ({ type: 'inc', amount }),
+ *   })
+ *
+ *   return (
+ *     <>
+ *       <div>
+ *         {counter}
+ *       </div>
+ *       <button onClick={inc1}>Increment by 1</button>
+ *       <button onClick={() => inc(5)}>Increment by 5</button>
+ *     </>
+ *   )
+ * }
  */
 export function useRedux(selector, actions) {
   return [useSelector(selector), useActions(actions)]
--- src/hooks/useReduxContext.js
+++ src/hooks/useReduxContext.js
@@ -5,20 +5,18 @@ import { ReactReduxContext } from '../components/Context'
 /**
  * A hook to access the value of the `ReactReduxContext`. This is a low-level
  * hook that you should usually not need to call directly.
- * 
+ *
  * @returns {any} the value of the `ReactReduxContext`
  *
- * Usage:
+ * @example
  *
-```jsx
-import React from 'react'
-import { useReduxContext } from 'react-redux'
-
-export const CounterComponent = ({ value }) => {
-  const { store } = useReduxContext()
-  return <div>{store.getState()}</div>
-}
-```
+ * import React from 'react'
+ * import { useReduxContext } from 'react-redux'
+ *
+ * export const CounterComponent = ({ value }) => {
+ *   const { store } = useReduxContext()
+ *   return <div>{store.getState()}</div>
+ * }
  */
 export function useReduxContext() {
   const contextValue = useContext(ReactReduxContext)
