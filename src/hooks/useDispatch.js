9f422c3e9afe887266e56310c8bad5afe3324967
Jonathan Ziller <jonathan.ziller@gmail.com>
2019-04-23 20:17:19 +0200

    use proper example code tag in hooks JSDoc comments (#1257)
    * use proper example code tag in hooks JSDoc comments
    
    * fix mistake in `useActions` hook example code
    
    * remove TypeScript annotations from example code and adjust `useReduxContext` hook to also use @example JSDoc tag

--- src/hooks/useDispatch.js
+++ src/hooks/useDispatch.js
@@ -4,26 +4,24 @@ import { useStore } from './useStore'
  * A hook to access the redux `dispatch` function. Note that in most cases where you
  * might want to use this hook it is recommended to use `useActions` instead to bind
  * action creators to the `dispatch` function.
- * 
+ *
  * @returns {any} redux store's `dispatch` function
  *
- * Usage:
+ * @example
  *
-```jsx
-import React, { useCallback } from 'react'
-import { useReduxDispatch } from 'react-redux'
-
-export const CounterComponent = ({ value }) => {
-  const dispatch = useDispatch()
-  const increaseCounter = useCallback(() => dispatch({ type: 'increase-counter' }), [])
-  return (
-    <div>
-      <span>{value}</span>
-      <button onClick={increaseCounter}>Increase counter</button>
-    </div>
-  )
-}
-```
+ * import React, { useCallback } from 'react'
+ * import { useReduxDispatch } from 'react-redux'
+ *
+ * export const CounterComponent = ({ value }) => {
+ *   const dispatch = useDispatch()
+ *   const increaseCounter = useCallback(() => dispatch({ type: 'increase-counter' }), [])
+ *   return (
+ *     <div>
+ *       <span>{value}</span>
+ *       <button onClick={increaseCounter}>Increase counter</button>
+ *     </div>
+ *   )
+ * }
  */
 export function useDispatch() {
   const store = useStore()
