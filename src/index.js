d808e84dbb226e730b4d2d9318303e8e3a3e399e
Josh <jcs.gnoff@gmail.com>
2019-05-06 15:44:03 -0700

    export hooks

--- src/index.js
+++ src/index.js
@@ -3,9 +3,22 @@ import connectAdvanced from './components/connectAdvanced'
 import { ReactReduxContext } from './components/Context'
 import connect from './connect/connect'
 
+import { useDispatch } from './hooks/useDispatch'
+import { useSelector } from './hooks/useSelector'
+import { useStore } from './hooks/useStore'
+
 import { setBatch } from './utils/batch'
 import { unstable_batchedUpdates as batch } from './utils/reactBatchedUpdates'
 
 setBatch(batch)
 
-export { Provider, connectAdvanced, ReactReduxContext, connect, batch }
+export {
+  Provider,
+  connectAdvanced,
+  ReactReduxContext,
+  connect,
+  batch,
+  useDispatch,
+  useSelector,
+  useStore
+}
