e49ceddfc2e2b35b98bd6723ff055b06ba69da88
alex <379118572@qq.com>
2019-06-05 15:14:11 +0800

    remove React

--- src/components/Provider.js
+++ src/components/Provider.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import { Component } from 'react'
 import PropTypes from 'prop-types'
 import { ReactReduxContext } from './Context'
 import Subscription from '../utils/Subscription'
