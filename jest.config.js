b832f835d76ec9d1fcfbdee6c80d497df5dbca06
Mark Erikson <mark@isquaredsoftware.com>
2019-11-05 21:36:42 -0500

    Fix RN batching and effect behavior (#1444)
    * Add React Native deps for testing
    
    * Add a Jest config to run tests in an RN environment
    
    * Add initial RN tests
    
    * Add test for RN batch export
    
    * Add jest-native testing assertions
    
    * Extract useIsomorphicLayoutEffect utility
    
    * Add additional RN batching-related tests
    
    * 7.1.2-alpha.0

---
+++ jest.config.js
@@ -0,0 +1,29 @@
+const defaults = {
+  coverageDirectory: './coverage/',
+  collectCoverage: true,
+  testURL: 'http://localhost'
+}
+
+const testFolderPath = folderName => `<rootDir>/test/${folderName}/**/*.js`
+
+const NORMAL_TEST_FOLDERS = ['components', 'hooks', 'integration', 'utils']
+
+const standardConfig = {
+  ...defaults,
+  displayName: 'ReactDOM',
+  testMatch: NORMAL_TEST_FOLDERS.map(testFolderPath)
+}
+
+const rnConfig = {
+  ...defaults,
+  displayName: 'React Native',
+  testMatch: [testFolderPath('react-native')],
+  preset: 'react-native',
+  transform: {
+    '^.+\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js'
+  }
+}
+
+module.exports = {
+  projects: [standardConfig, rnConfig]
+}
---
+++ src/utils/useIsomorphicLayoutEffect.native.js
@@ -0,0 +1,5 @@
+import { useLayoutEffect } from 'react'
+
+// Under React Native, we know that we always want to use useLayoutEffect
+
+export const useIsomorphicLayoutEffect = useLayoutEffect
