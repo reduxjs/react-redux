92fdd5b5f7daa71be715099c37b4876230738784
Phap <centralparkloop@yahoo.com>
2020-02-23 19:52:42 -0500

    add test for number

--- test/connect/mapDispatchToProps.spec.js
+++ test/connect/mapDispatchToProps.spec.js
@@ -4,6 +4,7 @@ describe('Connect', () => {
     describe('whenMapDispatchToPropsIsFunction', () => {
         it('should return undefined when parameter is not a function', () => {
             expect(whenMapDispatchToPropsIsFunction(true)).toBeUndefined()
+            expect(whenMapDispatchToPropsIsFunction(0)).toBeUndefined()
             expect(whenMapDispatchToPropsIsFunction({})).toBeUndefined()
             expect(whenMapDispatchToPropsIsFunction(undefined)).toBeUndefined()
             expect(whenMapDispatchToPropsIsFunction(null)).toBeUndefined()
@@ -14,6 +15,7 @@ describe('Connect', () => {
     describe('whenMapDispatchToPropsIsMissing', () => {
         it('should return undefined when truthy', () => {
             expect(whenMapDispatchToPropsIsMissing(true)).toBeUndefined()
+            expect(whenMapDispatchToPropsIsMissing(0)).toBeDefined()
             expect(whenMapDispatchToPropsIsMissing({})).toBeUndefined()
             expect(whenMapDispatchToPropsIsMissing(undefined)).toBeDefined()
             expect(whenMapDispatchToPropsIsMissing(null)).toBeDefined()
@@ -24,6 +26,7 @@ describe('Connect', () => {
     describe('whenMapDispatchToPropsIsObject', () => {
         it('should return undefined when parameter is not an object', () => {
             expect(whenMapDispatchToPropsIsObject(true)).toBeUndefined()
+            expect(whenMapDispatchToPropsIsObject(0)).toBeUndefined()
             expect(whenMapDispatchToPropsIsObject({})).toBeDefined()
             expect(whenMapDispatchToPropsIsObject(undefined)).toBeUndefined()
             expect(whenMapDispatchToPropsIsObject(null)).toBeUndefined()
