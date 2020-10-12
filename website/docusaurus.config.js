89e3c92a5d2daf96fb67010b095075221bc93d95
josephMG <joseph780116.mg@gmail.com>
2020-10-12 19:32:40 +0800

    Modify: change button--primary dark mode css

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -91,6 +91,11 @@ const siteConfig = {
         srcDark: 'img/redux_white.svg',
       },
       items: [
+        {
+          type: 'docsVersionDropdown',
+          position: 'left',
+          // Do not add the link active class when browsing docs.
+        },
         { to: 'introduction/quick-start', label: 'Quick Start', position: 'right' },
         { to: 'using-react-redux/connect-mapstate', label: 'Using React Redux', position: 'right' },
         { to: 'api/connect', label: 'API', position: 'right' },
