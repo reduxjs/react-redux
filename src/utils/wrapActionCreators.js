2efa6f8b1180d9c1b143c2d93290d03197325da2
Tianyu Xia <xty@users.noreply.github.com>
2021-04-09 10:50:26 +0800

    Remove wrapActionCreators
    Remove wrapActionCreators.js and its associated test file,
    as it is no longer used anywhere in the codebase.

--- src/utils/wrapActionCreators.js
+++
@@ -1,5 +0,0 @@
-import bindActionCreators from './bindActionCreators'
-
-export default function wrapActionCreators(actionCreators) {
-  return (dispatch) => bindActionCreators(actionCreators, dispatch)
-}
