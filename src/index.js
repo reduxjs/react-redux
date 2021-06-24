a9e65c9a3ceb0088d970417c1417a437e4effbca
Chuck Dries <chuckd@axosoft.com>
2021-06-24 07:56:06 -0700

    delete test.ts

--- src/index.js
+++ src/index.js
@@ -11,10 +11,6 @@ import { setBatch } from './utils/batch'
 import { unstable_batchedUpdates as batch } from './utils/reactBatchedUpdates'
 import shallowEqual from './utils/shallowEqual'
 
-import { add } from './test'
-
-console.log(add(3, 3))
-
 setBatch(batch)
 
 export {
