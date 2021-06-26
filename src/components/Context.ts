420891dd22e4d129e5fa9789c34c9e3e59f531da
Jeroen Vannevel <jer_vannevel@outlook.com>
2021-06-26 16:29:57 +0100

    Use types that came from the typedef package

--- src/components/Context.ts
+++ src/components/Context.ts
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Action, AnyAction, Store } from 'redux'
+import type { FixTypeLater } from '../types'
 import type Subscription from '../utils/Subscription'
 
-export type ReduxContextProps = {
-  store: any
+export interface ReactReduxContextValue<A extends Action = AnyAction> {
+  store: Store<FixTypeLater, A>;
   subscription: Subscription
 }
-export const ReactReduxContext = /*#__PURE__*/ React.createContext<ReduxContextProps | null>(null)
+
+export const ReactReduxContext = /*#__PURE__*/ React.createContext<ReactReduxContextValue | null>(null)
 
 if (process.env.NODE_ENV !== 'production') {
   ReactReduxContext.displayName = 'ReactRedux'
--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -1,10 +1,26 @@
-import React, { ReactNode, useMemo } from 'react'
+import React, { Context, ReactNode, useMemo } from 'react'
 import PropTypes from 'prop-types'
-import { ReactReduxContext, ReduxContextProps } from './Context'
+import { ReactReduxContext, ReactReduxContextValue } from './Context'
 import Subscription from '../utils/Subscription'
 import { useIsomorphicLayoutEffect } from '../utils/useIsomorphicLayoutEffect'
+import type { FixTypeLater } from '../types'
+import { Action, AnyAction, Store } from 'redux'
 
-function Provider({ store, context, children }: { store: any, context?: React.Context<ReduxContextProps>, children: ReactNode }) {
+interface ProviderProps<A extends Action = AnyAction> {
+  /**
+   * The single Redux store in your application.
+   */
+  store: Store<FixTypeLater, A>;
+  /**
+   * Optional context to be used internally in react-redux. Use React.createContext() to create a context to be used.
+   * If this is used, generate own connect HOC by using connectAdvanced, supplying the same context provided to the
+   * Provider. Initial value doesn't matter, as it is overwritten with the internal state of Provider.
+   */
+  context?: Context<ReactReduxContextValue>;
+  children: ReactNode
+}
+
+function Provider({ store, context, children }: ProviderProps) {
   const contextValue = useMemo(() => {
     const subscription = new Subscription(store)
     subscription.onStateChange = subscription.notifyNestedSubs
