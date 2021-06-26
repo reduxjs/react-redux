e961d2320e6e21073e30c9adb9f41fa11529620d
Jeroen Vannevel <jer_vannevel@outlook.com>
2021-06-26 18:18:22 +0100

    verifyPlainObject

--- src/utils/verifyPlainObject.ts
+++ src/utils/verifyPlainObject.ts
@@ -1,7 +1,7 @@
 import isPlainObject from './isPlainObject'
 import warning from './warning'
 
-export default function verifyPlainObject(value, displayName, methodName) {
+export default function verifyPlainObject(value: unknown, displayName: string, methodName: string) {
   if (!isPlainObject(value)) {
     warning(
       `${methodName}() in ${displayName} must return a plain object. Instead received ${value}.`
