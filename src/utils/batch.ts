14cf3e68b4e77e4d0f4805993eadbee35747245c
Jeroen Vannevel <jer_vannevel@outlook.com>
2021-06-26 18:29:44 +0100

    batch

--- src/utils/batch.ts
+++ src/utils/batch.ts
@@ -1,12 +1,12 @@
 // Default to a dummy "batch" implementation that just runs the callback
-function defaultNoopBatch(callback) {
+function defaultNoopBatch(callback: () => void) {
   callback()
 }
 
 let batch = defaultNoopBatch
 
 // Allow injecting another batching function later
-export const setBatch = (newBatch) => (batch = newBatch)
+export const setBatch = (newBatch: (callback: () => void) => void) => (batch = newBatch)
 
 // Supply a getter just to skip dealing with ESM bindings
 export const getBatch = () => batch
