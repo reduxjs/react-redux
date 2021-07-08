5331a1e743e63a3f71c4c77f3d5df3ec70e49f63
tony-go <gorez.tony@gmail.com>
2021-07-08 08:50:29 +0200

    fix: types

--- src/connect/mapDispatchToProps.ts
+++ src/connect/mapDispatchToProps.ts
@@ -1,25 +1,29 @@
-import { ActionCreator, ActionCreatorsMapObject, Dispatch } from 'redux'
+import { ActionCreatorsMapObject, Dispatch } from 'redux'
 import bindActionCreators from '../utils/bindActionCreators'
-import { wrapMapToPropsConstant, wrapMapToPropsFunc } from './wrapMapToProps'
+import {
+  MapToProps,
+  wrapMapToPropsConstant,
+  wrapMapToPropsFunc,
+} from './wrapMapToProps'
 
 export function whenMapDispatchToPropsIsFunction(
-  mapDispatchToProps: ActionCreatorsMapObject | ActionCreator<any>
+  mapDispatchToProps: ActionCreatorsMapObject | MapToProps
 ) {
   return typeof mapDispatchToProps === 'function'
     ? wrapMapToPropsFunc(mapDispatchToProps, 'mapDispatchToProps')
     : undefined
 }
 
-export function whenMapDispatchToPropsIsMissing(
-  mapDispatchToProps: ActionCreatorsMapObject | ActionCreator<any>
-) {
+export function whenMapDispatchToPropsIsMissing(mapDispatchToProps: undefined) {
   return !mapDispatchToProps
-    ? wrapMapToPropsConstant((dispatch) => ({ dispatch }))
+    ? wrapMapToPropsConstant((dispatch: Dispatch) => ({
+        dispatch,
+      }))
     : undefined
 }
 
 export function whenMapDispatchToPropsIsObject(
-  mapDispatchToProps: ActionCreatorsMapObject | ActionCreator<any>
+  mapDispatchToProps: ActionCreatorsMapObject
 ) {
   return mapDispatchToProps && typeof mapDispatchToProps === 'object'
     ? wrapMapToPropsConstant((dispatch: Dispatch) =>
