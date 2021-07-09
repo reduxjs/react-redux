2f83cf32124443fe487956b021775739dc90b4bc
Steven Langbroek <steven.langbroek@yara.com>
2021-07-09 09:05:50 +0200

    refactor(tests): add ts-jest dependencies & first test
    i've temporarily disabled rnConfig, will fix when the
    non-rn tests are all passing.

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,21 @@
+const { default: tsJestPreset } = require('ts-jest')
+
 const defaults = {
+  ...tsJestPreset,
   coverageDirectory: './coverage/',
   collectCoverage: true,
-  testURL: 'http://localhost'
+  testURL: 'http://localhost',
 }
 
-const testFolderPath = folderName => `<rootDir>/test/${folderName}/**/*.js`
+const testFolderPath = (folderName) =>
+  `<rootDir>/test/${folderName}/**/*.{js,ts,tsx}`
 
 const NORMAL_TEST_FOLDERS = ['components', 'hooks', 'integration', 'utils']
 
 const standardConfig = {
   ...defaults,
   displayName: 'ReactDOM',
-  testMatch: NORMAL_TEST_FOLDERS.map(testFolderPath)
+  testMatch: NORMAL_TEST_FOLDERS.map(testFolderPath),
 }
 
 const rnConfig = {
@@ -20,10 +24,10 @@ const rnConfig = {
   testMatch: [testFolderPath('react-native')],
   preset: 'react-native',
   transform: {
-    '^.+\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js'
-  }
+    '^.+\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
+  },
 }
 
 module.exports = {
-  projects: [standardConfig, rnConfig]
+  projects: [standardConfig],
 }
