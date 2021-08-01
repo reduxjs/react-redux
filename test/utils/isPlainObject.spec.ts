9e6523f0a63260b1ac613f6f9616fa9c0b06f56a
度 <53305259+myNameIsDu@users.noreply.github.com>
2021-08-02 03:31:49 +0800

    refactor: transform test/utils to ts (#1785)

--- test/utils/isPlainObject.spec.ts
+++ test/utils/isPlainObject.spec.ts
@@ -1,12 +1,8 @@
 import isPlainObject from '../../src/utils/isPlainObject'
 import vm from 'vm'
-
+class Test {}
 describe('isPlainObject', () => {
   it('returns true only if plain object', () => {
-    function Test() {
-      this.prop = 1
-    }
-
     const sandbox = { fromAnotherRealm: false }
     vm.runInNewContext('fromAnotherRealm = {}', sandbox)
 
@@ -15,6 +11,7 @@ describe('isPlainObject', () => {
     expect(isPlainObject(new Date())).toBe(false)
     expect(isPlainObject([1, 2, 3])).toBe(false)
     expect(isPlainObject(null)).toBe(false)
+    //@ts-expect-error
     expect(isPlainObject()).toBe(false)
     expect(isPlainObject({ x: 1, y: 2 })).toBe(true)
     expect(isPlainObject(Object.create(null))).toBe(true)
