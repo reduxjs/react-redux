ddab3b1b513b22e38a058580bcb91fe3a353bf49
Mark Erikson <mark@isquaredsoftware.com>
2021-12-21 17:39:12 -0500

    Fix types issues with Provider context prop

--- src/components/Provider.tsx
+++ src/components/Provider.tsx
@@ -2,24 +2,27 @@ import React, { Context, ReactNode, useMemo } from 'react'
 import { ReactReduxContext, ReactReduxContextValue } from './Context'
 import { createSubscription } from '../utils/Subscription'
 import { useIsomorphicLayoutEffect } from '../utils/useIsomorphicLayoutEffect'
-import type { FixTypeLater } from '../types'
 import { Action, AnyAction, Store } from 'redux'
 
 export interface ProviderProps<A extends Action = AnyAction> {
   /**
    * The single Redux store in your application.
    */
-  store: Store<FixTypeLater, A>
+  store: Store<any, A>
   /**
    * Optional context to be used internally in react-redux. Use React.createContext() to create a context to be used.
    * If this is used, you'll need to customize `connect` by supplying the same context provided to the Provider.
    * Initial value doesn't matter, as it is overwritten with the internal state of Provider.
    */
-  context?: Context<ReactReduxContextValue>
+  context?: Context<ReactReduxContextValue<any, A>>
   children: ReactNode
 }
 
-function Provider({ store, context, children }: ProviderProps) {
+function Provider<A extends Action = AnyAction>({
+  store,
+  context,
+  children,
+}: ProviderProps<A>) {
   const contextValue = useMemo(() => {
     const subscription = createSubscription(store)
     return {
@@ -46,6 +49,7 @@ function Provider({ store, context, children }: ProviderProps) {
 
   const Context = context || ReactReduxContext
 
+  // @ts-ignore 'AnyAction' is assignable to the constraint of type 'A', but 'A' could be instantiated with a different subtype
   return <Context.Provider value={contextValue}>{children}</Context.Provider>
 }
 
