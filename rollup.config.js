5c5d0e902a765a1fd1af35e76d80bbf28991d307
Gabriel Murry <63678558+GabrielMurry@users.noreply.github.com>
2023-04-08 17:07:57 +0000

    rollup-plugin-terser pkg deprecated. Switched to @rollup/plugin-terser

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import nodeResolve from '@rollup/plugin-node-resolve'
 import babel from '@rollup/plugin-babel'
 import replace from '@rollup/plugin-replace'
 import commonjs from '@rollup/plugin-commonjs'
-import { terser } from 'rollup-plugin-terser'
+import terser from '@rollup/plugin-terser'
 import pkg from './package.json'
 
 const env = process.env.NODE_ENV
