51cfc4c9c570646b0a9e64e8cde53c99473c15c5
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2023-06-05 22:52:44 +0200

    lazily create Context for RSC compat

--- src/components/Context.ts
+++ src/components/Context.ts
@@ -1,4 +1,4 @@
-import { createContext } from 'react'
+import { Context, createContext } from 'react'
 import type { Action, AnyAction, Store } from 'redux'
 import type { Subscription } from '../utils/Subscription'
 import { StabilityCheck } from '../hooks/useSelector'
@@ -13,13 +13,31 @@ export interface ReactReduxContextValue<
   stabilityCheck: StabilityCheck
 }
 
-export const ReactReduxContext =
-  /*#__PURE__*/ createContext<ReactReduxContextValue>(null as any)
+let realContext: Context<ReactReduxContextValue> | null = null
+function getContext() {
+  if (!realContext) {
+    realContext = createContext<ReactReduxContextValue>(null as any)
+    if (process.env.NODE_ENV !== 'production') {
+      realContext.displayName = 'ReactRedux'
+    }
+  }
+  return realContext
+}
+
+export const ReactReduxContext = /*#__PURE__*/ new Proxy(
+  {} as Context<ReactReduxContextValue>,
+  new Proxy<ProxyHandler<Context<ReactReduxContextValue>>>(
+    {},
+    {
+      get(_, handler) {
+        const target = getContext()
+        // @ts-ignore
+        return (_target, ...args) => Reflect[handler](target, ...args)
+      },
+    }
+  )
+)
 
 export type ReactReduxContextInstance = typeof ReactReduxContext
-
-if (process.env.NODE_ENV !== 'production') {
-  ReactReduxContext.displayName = 'ReactRedux'
-}
 
 export default ReactReduxContext
