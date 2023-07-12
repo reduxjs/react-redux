b37e57e9eb8981b03b92a09c58bd15664cb64a12
Lenz Weber-Tronic <mail@lenzw.de>
2023-07-12 11:46:38 +0200

    addresses #2053

--- src/components/Context.ts
+++ src/components/Context.ts
@@ -16,9 +16,9 @@ export interface ReactReduxContextValue<
 }
 
 const ContextKey = Symbol.for(`react-redux-context`)
-const gT = globalThis as {
+const gT: {
   [ContextKey]?: Map<typeof createContext, Context<ReactReduxContextValue>>
-}
+} = (typeof globalThis !== "undefined" ? globalThis : /* fall back to a per-module scope (pre-8.1 behaviour) if `globalThis` is not available */ {}) as any; 
 
 function getContext() {
   const contextMap = (gT[ContextKey] ??= new Map())
