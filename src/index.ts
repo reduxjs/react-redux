a0c7a4b3a73439eb1844b1b824cfb7431fa6fc19
Mark Erikson <mark@isquaredsoftware.com>
2023-09-25 10:26:15 -0400

    Fix uSES imports

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
-'use client'
-
 // The primary entry point assumes we are working with React 18, and thus have
 // useSyncExternalStore available. We can import that directly from React itself.
 // The useSyncExternalStoreWithSelector has to be imported, but we can use the
 // non-shim version. This shaves off the byte size of the shim.
 
 import * as React from 'react'
-import { useSyncExternalStoreWithSelector } from 'use-sync-external-store/with-selector'
+import { useSyncExternalStoreWithSelector } from 'use-sync-external-store/with-selector.js'
 
 import { unstable_batchedUpdates as batchInternal } from './utils/reactBatchedUpdates'
 import { setBatch } from './utils/batch'
