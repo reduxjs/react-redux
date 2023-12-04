fd2631f86110128d75b8170529ae20398f215249
Mark Erikson <mark@isquaredsoftware.com>
2023-12-04 15:00:09 -0500

    Really hack around React export

--- src/utils/react.ts
+++ src/utils/react.ts
@@ -1,6 +1,7 @@
 import * as ReactOriginal from 'react'
 import type * as ReactNamespace from 'react'
 
-export const React = (
-  'default' in ReactOriginal ? ReactOriginal['default'] : ReactOriginal
-) as typeof ReactNamespace
+export const React: typeof ReactNamespace =
+  // prettier-ignore
+  // @ts-ignore
+  'default' in ReactOriginal ? ReactOriginal['default'] : ReactOriginal as any
