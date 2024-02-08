e813464f7aa3f8f8edba316266e471aa438fea1f
Arya Emami <aryaemami59@yahoo.com>
2024-02-08 03:37:07 -0600

    MIgrate type tests for `provider.test-d.tsx` to Vitest

--- test/typetests/provider.test-d.tsx
+++ test/typetests/provider.test-d.tsx
@@ -1,19 +1,24 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
-import React from 'react'
 import type { Store } from 'redux'
 import { Provider } from '../../src'
 
 declare const store: Store<{ foo: string }>
 
-function App() {
-  return (
-    <Provider
-      store={store}
-      // @ts-expect-error
-      serverState={'oops'}
-    >
-      foo
-    </Provider>
-  )
-}
+describe('type tests', () => {
+  test('Provider', () => {
+    expectTypeOf(Provider)
+      .parameter(0)
+      .not.toMatchTypeOf({ store, serverState: 'oops', children: 'foo' })
+
+    const App = () => {
+      return (
+        <Provider
+          store={store}
+          // @ts-expect-error
+          serverState={'oops'}
+        >
+          foo
+        </Provider>
+      )
+    }
+  })
+})
