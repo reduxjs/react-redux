8ead8e269ef4bcf6e732695e0060f4aae86dfde7
Arya Emami <aryaemami59@yahoo.com>
2024-02-09 09:07:29 -0600

    Remove `expectType`

--- test/typeTestHelpers.ts
+++ test/typeTestHelpers.ts
@@ -1,27 +1,3 @@
-/**
- * return True if T is `any`, otherwise return False
- * taken from https://github.com/joonhocho/tsdef
- *
- * @internal
- */
-export type IsAny<T, True, False = never> =
-  // test if we are going the left AND right path in the condition
-  true | false extends (T extends never ? true : false) ? True : False
-
-/**
- * return True if T is `unknown`, otherwise return False
- * taken from https://github.com/joonhocho/tsdef
- *
- * @internal
- */
-export type IsUnknown<T, True, False = never> = unknown extends T
-  ? IsAny<T, False, True>
-  : False
-
-export function expectType<T>(t: T): T {
-  return t
-}
-
 export type IsEqual<A, B> = (<G>() => G extends A ? 1 : 2) extends <
   G,
 >() => G extends B ? 1 : 2
