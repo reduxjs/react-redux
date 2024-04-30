ab027bc422aa95cd79b621ee34cc54db2a839675
Arya Emami <aryaemami59@yahoo.com>
2024-04-29 22:35:48 -0500

    Fix `useRef` usages to be called with an explicit `undefined` argument.
      - This change was made by running `npx types-react-codemod useRef-required-initial .` in preparation for React 19.

--- src/components/connect.tsx
+++ src/components/connect.tsx
@@ -641,13 +641,13 @@ function connect<
       }, [didStoreComeFromProps, contextValue, subscription])
 
       // Set up refs to coordinate values between the subscription effect and the render logic
-      const lastChildProps = React.useRef<unknown>()
+      const lastChildProps = React.useRef<unknown>(undefined)
       const lastWrapperProps = React.useRef(wrapperProps)
-      const childPropsFromStoreUpdate = React.useRef<unknown>()
+      const childPropsFromStoreUpdate = React.useRef<unknown>(undefined)
       const renderIsScheduled = React.useRef(false)
       const isMounted = React.useRef(false)
 
-      const latestSubscriptionCallbackError = React.useRef<Error>()
+      const latestSubscriptionCallbackError = React.useRef<Error>(undefined)
 
       useIsomorphicLayoutEffect(() => {
         isMounted.current = true
@@ -752,11 +752,11 @@ function connect<
       const renderedWrappedComponent = React.useMemo(() => {
         return (
           // @ts-ignore
-          <WrappedComponent
+          (<WrappedComponent
             {...actualChildProps}
             ref={reactReduxForwardedRef}
-          />
-        )
+          />)
+        );
       }, [reactReduxForwardedRef, WrappedComponent, actualChildProps])
 
       // If React sees the exact same element reference as last time, it bails out of re-rendering
