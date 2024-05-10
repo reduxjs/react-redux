db8f22747f8de4f3c917c80c1c0861ae48597ec0
Arya Emami <aryaemami59@yahoo.com>
2024-05-10 16:15:38 -0500

    Update `react-is` implementation
    - `react-is` has changed the `REACT_ELEMENT_TYPE` symbol from `'react.element'` to `'react.transitional.element'`. We want our changes to be non-breaking and backwards-compatible so we conditionally set the `REACT_ELEMENT_TYPE` based on the detected version of React.

--- src/utils/react-is.ts
+++ src/utils/react-is.ts
@@ -1,111 +1,95 @@
 import type { ElementType, MemoExoticComponent, ReactElement } from 'react'
+import * as React from 'react'
 
 // Directly ported from:
-// https://unpkg.com/browse/react-is@18.3.0-canary-ee68446ff-20231115/cjs/react-is.production.js
+// https://unpkg.com/browse/react-is@19.0.0/cjs/react-is.production.js
 // It's very possible this could change in the future, but given that
 // we only use these in `connect`, this is a low priority.
 
-const REACT_ELEMENT_TYPE = Symbol.for('react.element')
-const REACT_PORTAL_TYPE = Symbol.for('react.portal')
-const REACT_FRAGMENT_TYPE = Symbol.for('react.fragment')
-const REACT_STRICT_MODE_TYPE = Symbol.for('react.strict_mode')
-const REACT_PROFILER_TYPE = Symbol.for('react.profiler')
-const REACT_PROVIDER_TYPE = Symbol.for('react.provider')
-const REACT_CONTEXT_TYPE = Symbol.for('react.context')
-const REACT_SERVER_CONTEXT_TYPE = Symbol.for('react.server_context')
-const REACT_FORWARD_REF_TYPE = Symbol.for('react.forward_ref')
-const REACT_SUSPENSE_TYPE = Symbol.for('react.suspense')
-const REACT_SUSPENSE_LIST_TYPE = Symbol.for('react.suspense_list')
-const REACT_MEMO_TYPE = Symbol.for('react.memo')
-const REACT_LAZY_TYPE = Symbol.for('react.lazy')
-const REACT_OFFSCREEN_TYPE = Symbol.for('react.offscreen')
-const REACT_CLIENT_REFERENCE = Symbol.for('react.client.reference')
+export const IS_REACT_19 = /* @__PURE__ */ React.version.startsWith('19')
+
+const REACT_ELEMENT_TYPE = /* @__PURE__ */ Symbol.for(
+  IS_REACT_19 ? 'react.transitional.element' : 'react.element',
+)
+const REACT_PORTAL_TYPE = /* @__PURE__ */ Symbol.for('react.portal')
+const REACT_FRAGMENT_TYPE = /* @__PURE__ */ Symbol.for('react.fragment')
+const REACT_STRICT_MODE_TYPE = /* @__PURE__ */ Symbol.for('react.strict_mode')
+const REACT_PROFILER_TYPE = /* @__PURE__ */ Symbol.for('react.profiler')
+const REACT_CONSUMER_TYPE = /* @__PURE__ */ Symbol.for('react.consumer')
+const REACT_CONTEXT_TYPE = /* @__PURE__ */ Symbol.for('react.context')
+const REACT_FORWARD_REF_TYPE = /* @__PURE__ */ Symbol.for('react.forward_ref')
+const REACT_SUSPENSE_TYPE = /* @__PURE__ */ Symbol.for('react.suspense')
+const REACT_SUSPENSE_LIST_TYPE = /* @__PURE__ */ Symbol.for(
+  'react.suspense_list',
+)
+const REACT_MEMO_TYPE = /* @__PURE__ */ Symbol.for('react.memo')
+const REACT_LAZY_TYPE = /* @__PURE__ */ Symbol.for('react.lazy')
+const REACT_OFFSCREEN_TYPE = /* @__PURE__ */ Symbol.for('react.offscreen')
+const REACT_CLIENT_REFERENCE = /* @__PURE__ */ Symbol.for(
+  'react.client.reference',
+)
 
 export const ForwardRef = REACT_FORWARD_REF_TYPE
 export const Memo = REACT_MEMO_TYPE
 
 export function isValidElementType(type: any): type is ElementType {
-  if (typeof type === 'string' || typeof type === 'function') {
-    return true
-  } // Note: typeof might be other than 'symbol' or 'number' (e.g. if it's a polyfill).
-
-  if (
+  return typeof type === 'string' ||
+    typeof type === 'function' ||
     type === REACT_FRAGMENT_TYPE ||
     type === REACT_PROFILER_TYPE ||
     type === REACT_STRICT_MODE_TYPE ||
     type === REACT_SUSPENSE_TYPE ||
     type === REACT_SUSPENSE_LIST_TYPE ||
-    type === REACT_OFFSCREEN_TYPE
-  ) {
-    return true
-  }
-
-  if (typeof type === 'object' && type !== null) {
-    if (
-      type.$$typeof === REACT_LAZY_TYPE ||
-      type.$$typeof === REACT_MEMO_TYPE ||
-      type.$$typeof === REACT_PROVIDER_TYPE ||
-      type.$$typeof === REACT_CONTEXT_TYPE ||
-      type.$$typeof === REACT_FORWARD_REF_TYPE || // This needs to include all possible module reference object
-      // types supported by any Flight configuration anywhere since
-      // we don't know which Flight build this will end up being used
-      // with.
-      type.$$typeof === REACT_CLIENT_REFERENCE ||
-      type.getModuleId !== undefined
-    ) {
-      return true
-    }
-  }
-
-  return false
+    type === REACT_OFFSCREEN_TYPE ||
+    (typeof type === 'object' &&
+      type !== null &&
+      (type.$$typeof === REACT_LAZY_TYPE ||
+        type.$$typeof === REACT_MEMO_TYPE ||
+        type.$$typeof === REACT_CONTEXT_TYPE ||
+        type.$$typeof === REACT_CONSUMER_TYPE ||
+        type.$$typeof === REACT_FORWARD_REF_TYPE ||
+        type.$$typeof === REACT_CLIENT_REFERENCE ||
+        type.getModuleId !== undefined))
+    ? !0
+    : !1
 }
 
 function typeOf(object: any): symbol | undefined {
   if (typeof object === 'object' && object !== null) {
-    const $$typeof = object.$$typeof
+    const { $$typeof } = object
 
     switch ($$typeof) {
-      case REACT_ELEMENT_TYPE: {
-        const type = object.type
-
-        switch (type) {
+      case REACT_ELEMENT_TYPE:
+        switch (((object = object.type), object)) {
           case REACT_FRAGMENT_TYPE:
           case REACT_PROFILER_TYPE:
           case REACT_STRICT_MODE_TYPE:
           case REACT_SUSPENSE_TYPE:
           case REACT_SUSPENSE_LIST_TYPE:
-            return type
-
-          default: {
-            const $$typeofType = type && type.$$typeof
-
-            switch ($$typeofType) {
-              case REACT_SERVER_CONTEXT_TYPE:
+            return object
+          default:
+            switch (((object = object && object.$$typeof), object)) {
               case REACT_CONTEXT_TYPE:
               case REACT_FORWARD_REF_TYPE:
               case REACT_LAZY_TYPE:
               case REACT_MEMO_TYPE:
-              case REACT_PROVIDER_TYPE:
-                return $$typeofType
-
+                return object
+              case REACT_CONSUMER_TYPE:
+                return object
               default:
                 return $$typeof
             }
-          }
         }
-      }
-
-      case REACT_PORTAL_TYPE: {
+      case REACT_PORTAL_TYPE:
         return $$typeof
-      }
     }
   }
-
-  return undefined
 }
 
 export function isContextConsumer(object: any): object is ReactElement {
-  return typeOf(object) === REACT_CONTEXT_TYPE
+  return IS_REACT_19
+    ? typeOf(object) === REACT_CONSUMER_TYPE
+    : typeOf(object) === REACT_CONTEXT_TYPE
 }
 
 export function isMemo(object: any): object is MemoExoticComponent<any> {
