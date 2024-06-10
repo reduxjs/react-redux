623b61add537856d50c1c622593a3375661635da
Arya Emami <aryaemami59@yahoo.com>
2024-06-10 14:49:08 -0500

    Fix `React` namespace import
    - The conditional import of the `React` namespace seems to create a lot of side effects, so for now we'll de-duplicate the `React` namespace imports by funneling them through `src/utils/react.ts`.

--- src/utils/react.ts
+++ src/utils/react.ts
@@ -1,6 +1,3 @@
-import type * as ReactNamespace from 'react'
-import * as ReactOriginal from 'react'
+import * as React from 'react'
 
-export const React: typeof ReactNamespace =
-  // prettier-ignore
-  'default' in ReactOriginal ? ReactOriginal['default'] : ReactOriginal as any
+export { React }
