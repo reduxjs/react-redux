b07d821078020f1819c9e0d8ea2f89e714f1ebc7
Arya Emami <aryaemami59@yahoo.com>
2024-06-25 09:15:30 -0500

    Remove `initializeConnect` and `initializeUseSelector`

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,1 @@
-// The primary entry point assumes we are working with React 18, and thus have
-// useSyncExternalStore available. We can import that directly from React itself.
-// The useSyncExternalStoreWithSelector has to be imported, but we can use the
-// non-shim version. This shaves off the byte size of the shim.
-
-import { React } from './utils/react'
-import { useSyncExternalStoreWithSelector } from 'use-sync-external-store/with-selector.js'
-
-import { initializeUseSelector } from './hooks/useSelector'
-import { initializeConnect } from './components/connect'
-
-initializeUseSelector(useSyncExternalStoreWithSelector)
-initializeConnect(React.useSyncExternalStore)
-
 export * from './exports'
