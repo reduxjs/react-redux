b4d5bdecfa1ffa9fb76bf4ec05ae484664d54771
Mark Erikson <mark@isquaredsoftware.com>
2026-06-08 23:18:05 -0400

    Export SignalProvider and useSignalSelector

--- src/exports.ts
+++ src/exports.ts
@@ -9,6 +9,7 @@ import shallowEqual from './utils/shallowEqual'
 
 import Provider from './components/Provider'
 import { defaultNoopBatch } from './utils/batch'
+import { SignalProvider, useSignalSelector } from './signals'
 
 export { ReactReduxContext } from './components/Context'
 export type { ReactReduxContextValue } from './components/Context'
@@ -48,4 +49,12 @@ export * from './types'
  */
 const batch = defaultNoopBatch
 
-export { Provider, batch, connect, legacy_connect, shallowEqual }
+export {
+  Provider,
+  batch,
+  connect,
+  legacy_connect,
+  shallowEqual,
+  SignalProvider,
+  useSignalSelector,
+}
